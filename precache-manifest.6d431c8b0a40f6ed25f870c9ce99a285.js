7242b701b972f86f18389a74ed900db218bcb519
Duy Phuong Nguyen <maconline@Phuongs-Mac.local>
2020-08-14 23:51:10 +0700

    Updates

---
+++ precache-manifest.6d431c8b0a40f6ed25f870c9ce99a285.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69638b3b9b27a1561a3e0e6a58975541",
+    "url": "/my_react_app/index.html"
+  },
+  {
+    "revision": "222c2ab00d4ed5d12fbc",
+    "url": "/my_react_app/static/css/2.b538f0f7.chunk.css"
+  },
+  {
+    "revision": "6a1a71653862b63561ce",
+    "url": "/my_react_app/static/css/main.6b597a14.chunk.css"
+  },
+  {
+    "revision": "222c2ab00d4ed5d12fbc",
+    "url": "/my_react_app/static/js/2.6f231ec1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my_react_app/static/js/2.6f231ec1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a1a71653862b63561ce",
+    "url": "/my_react_app/static/js/main.d648378f.chunk.js"
+  },
+  {
+    "revision": "27bf8a2ee591f3e07137",
+    "url": "/my_react_app/static/js/runtime-main.2c3f29c5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/my_react_app/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/my_react_app/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/my_react_app/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/my_react_app/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/my_react_app/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
